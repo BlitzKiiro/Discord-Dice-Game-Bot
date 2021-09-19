2eb170a16db5f9f8ab83a37138936df8e4deb3bc
BlitzKiiro <ahmedazmycs@protonmail.com>
2021-09-19 21:21:00 +0200

    new play command

--- commands/roll game/game functions/channelCreator.js
+++ commands/roll game/game functions/channelCreator.js
@@ -1,34 +1,18 @@
-module.exports =  async (message,players) => {
-    const server = message.guild
-    return await server.channels.create( `${message.author.username.toString()} game` , {
-        type : 'text',
-        parent: server.channels.cache.filter(channel => channel.name === 'Potato Roulette').array()[0],
-        permissionOverwrites : [
-            {
-                id: server.roles.everyone,
-                deny : [
-                    'VIEW_CHANNEL',
-                    'SEND_MESSAGES',
-                ]
-            },
-            {
-                id: message.author.id,
-                allow : [
-                    'VIEW_CHANNEL',
-                    'SEND_MESSAGES',
-                ]
-            }
-        ]
-    } ).then(
-        playing_channel => {
-            players.forEach( player => {
-                playing_channel.updateOverwrite( player ,
-                    { 
-                        VIEW_CHANNEL: true,
-                        SEND_MESSAGES : true,
-                });
-            }) 
-            return playing_channel
-        }
-    )
-}
+module.exports = async (message) => {
+  const server = message.guild;
+  return await server.channels.create(
+    `${message.author.username.toString()} game`,
+    {
+      type: "text",
+      parent: server.channels.cache
+        .filter((channel) => channel.name === "Potato Roulette")
+        .array()[0],
+      permissionOverwrites: [
+        {
+          id: server.roles.everyone,
+          deny: ["SEND_MESSAGES"],
+        },
+      ],
+    }
+  );
+};
--- commands/roll game/game functions/startingReact.js
+++ commands/roll game/game functions/startingReact.js
@@ -1,9 +1,12 @@
-module.exports = async (message,players_count) => {
-    try {
-        const filter = reaction  => reaction.emoji.name === '👌'
-        const collected_reactions = await message.awaitReactions( filter , { time: 60000 , max: players_count+1  })
-        return collected_reactions
-    } catch (error) {
-        console.log(error)
-    }
-}
+module.exports = async (message) => {
+  try {
+    const filter = (reaction) => reaction.emoji.name === "👌";
+    const collected_reactions = await message.awaitReactions(filter, {
+      time: 30000,
+      max: 6,
+    });
+    return collected_reactions;
+  } catch (error) {
+    console.log(error);
+  }
+};
