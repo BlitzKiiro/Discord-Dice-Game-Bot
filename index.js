01630c8defe535457a49e87ffcd42782902447eb
Ahmed <ahmedazmycs@protonmail.com>
2022-07-28 16:54:57 +0200

    Heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const {token} = require('./config.json')
-const Discord = require('discord.js');
-const Database = require('./prisma/interface')
+const fs = require("fs");
+const { token } = require("./config.json");
+const Discord = require("discord.js");
+const Database = require("./prisma/Database");
 
 //setuping up discord js variables
 const client = new Discord.Client();
@@ -9,36 +9,39 @@ client.commands = new Discord.Collection();
 
 //global  variables
 global.__root = __dirname;
-global.__commands = null
+global.__commands = null;
 
 //reading commands folder
-const commandFolders = fs.readdirSync('./commands');
+const commandFolders = fs.readdirSync("./commands");
 
 for (const folder of commandFolders) {
-	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const command = require(`./commands/${folder}/${file}`);
-		client.commands.set(command.name, command);
-	}
-	__commands = client.commands;
-
+  const commandFiles = fs
+    .readdirSync(`./commands/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    client.commands.set(command.name, command);
+  }
+  __commands = client.commands;
 }
 
-//intialize Database 
-const database = new Database()
+//intialize Database
+const database = new Database();
 
 //reading events folder
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args,client));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args,client,database));
-	}
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) =>
+      event.execute(...args, client, database)
+    );
+  }
 }
-
-
 
 client.login(token);
